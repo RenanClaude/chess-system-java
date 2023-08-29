5a08c9538a461f83114d304ba5f371290b26ccc5
Renan Claude <renanclaude.rcg@gmail.com>
2023-08-28 22:50:16 -0300

    Implementing possible moves of Rook

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -17,6 +18,49 @@ public class Rook extends ChessPiece {
 
   @Override
   public boolean[][] possibleMoves() {
-    return new boolean[this.getBoard().getRows()][this.getBoard().getColumns()];
+    boolean[][] matriz = new boolean[this.getBoard().getRows()][this.getBoard().getColumns()];
+    Position pos = new Position(0, 0);
+
+    //Above
+    pos.setValues(this.position.getRow() - 1, this.position.getColumn());
+    while (getBoard().positionExistis(pos) && !getBoard().thereIsAPieace(pos)) {
+      matriz[pos.getRow()][pos.getColumn()] = true;
+      pos.setRow(pos.getRow() - 1);
+    }
+    if (getBoard().positionExistis(pos) && isThereOpponentPiece(pos)) {
+      matriz[pos.getRow()][pos.getColumn()] = true;
+    }
+
+    //Left
+    pos.setValues(this.position.getRow(), this.position.getColumn() - 1);
+    while (getBoard().positionExistis(pos) && !getBoard().thereIsAPieace(pos)) {
+      matriz[pos.getRow()][pos.getColumn()] = true;
+      pos.setColumn(pos.getColumn() - 1);
+    }
+    if (getBoard().positionExistis(pos) && isThereOpponentPiece(pos)) {
+      matriz[pos.getRow()][pos.getColumn()] = true;
+    }
+
+    //Right
+    pos.setValues(this.position.getRow(), this.position.getColumn() + 1);
+    while (getBoard().positionExistis(pos) && !getBoard().thereIsAPieace(pos)) {
+      matriz[pos.getRow()][pos.getColumn()] = true;
+      pos.setColumn(pos.getColumn() + 1);
+    }
+    if (getBoard().positionExistis(pos) && isThereOpponentPiece(pos)) {
+      matriz[pos.getRow()][pos.getColumn()] = true;
+    }
+
+    //Below
+    pos.setValues(this.position.getRow() + 1, this.position.getColumn());
+    while (getBoard().positionExistis(pos) && !getBoard().thereIsAPieace(pos)) {
+      matriz[pos.getRow()][pos.getColumn()] = true;
+      pos.setRow(pos.getRow() + 1);
+    }
+    if (getBoard().positionExistis(pos) && isThereOpponentPiece(pos)) {
+      matriz[pos.getRow()][pos.getColumn()] = true;
+    }
+
+    return matriz;
   }
 }
